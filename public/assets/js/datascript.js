9e52f36a969c6bf0457333504f525886e2099fe1
fadhilatur7 <fadhilatur7@gmail.com>
2020-05-27 21:38:47 +0700

    convert date javascript

--- public/assets/js/datascript.js
+++ public/assets/js/datascript.js
@@ -1,5 +1,22 @@
 
    $(document).ready(function (){
+	// convert date
+	$.date = function(dateObject) {
+		var d = new Date(dateObject * 1000);
+		var day = d.getDate();
+		var month = d.getMonth() + 1;
+		var year = d.getFullYear();
+		if (day < 10) {
+			day = "0" + day;
+		}
+		if (month < 10) {
+			month = "0" + month;
+		}
+		var date = year + "-" + month + "-" + day;
+	
+		return date;
+	};
+	// end date
     domain = document.URL;
    	var limit = 10;
    	$('.loadmore').click(function (){
@@ -14,8 +31,9 @@
    			dataType : "JSON",
    		success: function(response){
    			var getmore = response;
-   			var html = '';
+			var html = '';
    			for (let a = 0; a < getmore.length; a++) {
+				// alert($.date(getmore[a].published_at));
    				html += '<div class="row">'+
    				'<div class="col-50">'+
    					'<div class="content">'+
@@ -28,7 +46,7 @@
    					'<div class="content-text">'+
    						'<span>'+getmore[a].author+'</span>'+
    						'<a class="external" href="'+domain+'detail/'+getmore[a].id+'/'+getmore[a].slug+' "><h5>'+getmore[a].title+'</h5></a>'+
-   						'<p class="date">20 minute ago</p>'+
+   						'<p class="date">'+$.date(getmore[a].published_at)+'</p>'+
    					'</div>'+
    				'</div>'+
    			'</div>';
--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,5 +1,22 @@
 
    $(document).ready(function (){
+	// convert date
+	$.date = function(dateObject) {
+		var d = new Date(dateObject * 1000);
+		var day = d.getDate();
+		var month = d.getMonth() + 1;
+		var year = d.getFullYear();
+		if (day < 10) {
+			day = "0" + day;
+		}
+		if (month < 10) {
+			month = "0" + month;
+		}
+		var date = year + "-" + month + "-" + day;
+	
+		return date;
+	};
+	// end date
     domain = $("#baseurl").val();
    	var limit = 10;
    	$('.loadmore').click(function (){
@@ -28,7 +45,7 @@
    					'<div class="content-text">'+
    						'<span>'+getmore[a].author+'</span>'+
    						'<a class="external" href="'+domain+'/detail/'+getmore[a].id+'/'+getmore[a].slug+' "><h5>'+getmore[a].title+'</h5></a>'+
-   						'<p class="date">20 minute ago</p>'+
+   						'<p class="date">'+$.date(getmore[a].published_at)+'</p>'+
    					'</div>'+
    				'</div>'+
    			'</div>';
