5acaeee1d3076ef778ea31bc611193dcd604bf7d
fadhilatur7 <fadhilatur7@gmail.com>
2020-05-31 06:39:45 +0700

    google analityc

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,9 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
+<script src="{{ asset('assets/js/framework7.js') }}"></script>
+	<script src="{{ asset('assets/js/routes.js') }}"></script>
+	<script src="{{ asset('assets/js/app.js') }}"></script>
+	<script src="{{ asset('assets/js/styleswitcher.js') }}"></script>
+	<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-167976221-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
@@ -7,11 +12,5 @@
 
   gtag('config', 'UA-167976221-1');
 </script>
-
-<script src="{{ asset('assets/js/framework7.js') }}"></script>
-	<script src="{{ asset('assets/js/routes.js') }}"></script>
-	<script src="{{ asset('assets/js/app.js') }}"></script>
-	<script src="{{ asset('assets/js/styleswitcher.js') }}"></script>
-	<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
 </body>
 </html>
